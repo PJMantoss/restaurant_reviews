b297a3f76a57d5c6f879b9a741040ca915e6ffae
PJMantoss <joelptoss@gmail.com>
2021-07-04 19:50:36 +0100

    update reviews controller

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -1,6 +1,8 @@
 import ReviewsDAO from '../dao/reviewsDAO.js';
 
 export default class ReviewsController {
+    static async apiGetRestaurants(req, res, next){}
+    
     static async apiPostReview(req, res, next){
         try{
             const restaurantId = req.body.restaurant_id;
