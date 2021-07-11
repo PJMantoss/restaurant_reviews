130e5fb168d5f15724b81ce61f3575a64f2285a9
PJMantoss <joelptoss@gmail.com>
2021-07-11 15:57:29 +0100

    update restaurants controller

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -24,26 +24,26 @@ export default class RestaurantsController {
             restaurants: restaurantsList,
             page: page,
             filters: filters,
-            entries_per_page: restuarantsPerPage,
+            entries_per_page: restaurantsPerPage,
             total_results: totalNumRestaurants
         }
         res.json(response);
     }
 
-    // static async apiGetRestaurantById(req, res, next){
-    //     try{
-    //         let id = req.params.id || {}
-    //         let restaurant = await RestaurantsDAO.getRestaurantByID(id);
-    //         if(!restaurant){
-    //             res.status(404).json({ error: "Not Found" });
-    //             return
-    //         }
-    //         res.json(restaurant)
-    //     }catch(e){
-    //         console.log(`api, ${e}`);
-    //         res.status(500).json({ error: e });
-    //     }
-    // }
+    static async apiGetRestaurantById(req, res, next){
+        try{
+            let id = req.params.id || {}
+            let restaurant = await RestaurantsDAO.getRestaurantByID(id);
+            if(!restaurant){
+                res.status(404).json({ error: "Not Found" });
+                return
+            }
+            res.json(restaurant)
+        }catch(e){
+            console.log(`api, ${e}`);
+            res.status(500).json({ error: e });
+        }
+    }
 
     // static async apiGetRestaurantCuisines(req, res, next){
     //     try{
