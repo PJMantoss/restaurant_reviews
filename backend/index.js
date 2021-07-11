5c99a3e9aa7080166e9fe35fabf44bcedcfc3811
PJMantoss <joelptoss@gmail.com>
2021-07-11 17:58:44 +0100

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000;
 
-console.log(process.env.RESTREVIEWS_DB_URI);
+//console.log(process.env.RESTREVIEWS_DB_URI);
 
 MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
