ba044030a7f37db1bdf086c36e41f322689cee71
PJMantoss <joelptoss@gmail.com>
2022-01-23 19:31:06 +0100

    update restaurants list

--- frontend/src/components/RestaurantsList.js
+++ frontend/src/components/RestaurantsList.js
@@ -9,6 +9,27 @@ const RestaurantsList = props => {
     const [searchCuisine, setSearchCuisine ] = useState("");
     const [cuisines, setCuisines] = useState(["All Cuisines"]);
 
+    useEffect(() => {
+        retrieveRestaurants();
+        retrieveCuisines();
+      }, []);
+    
+      const onChangeSearchName = e => {
+        const searchName = e.target.value;
+        setSearchName(searchName);
+      };
+    
+      const onChangeSearchZip = e => {
+        const searchZip = e.target.value;
+        setSearchZip(searchZip);
+      };
+    
+      const onChangeSearchCuisine = e => {
+        const searchCuisine = e.target.value;
+        setSearchCuisine(searchCuisine);
+        
+      };
+
   return (
     <div className="App">
       Hello Universe!
