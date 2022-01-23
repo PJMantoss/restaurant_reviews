3f2e0b24e13722110fcf352ddfee5e780c259987
PJMantoss <joelptoss@gmail.com>
2022-01-23 19:30:11 +0100

    update restaurants list

--- frontend/src/components/RestaurantsList.js
+++ frontend/src/components/RestaurantsList.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
 
-function RestaurantsList() {
+const RestaurantsList = props => {
+    const [restaurants, setRestaurants] = useState([]);
+    const [searchName, setSearchName ] = useState("");
+    const [searchZip, setSearchZip ] = useState("");
+    const [searchCuisine, setSearchCuisine ] = useState("");
+    const [cuisines, setCuisines] = useState(["All Cuisines"]);
+
   return (
     <div className="App">
       Hello Universe!
