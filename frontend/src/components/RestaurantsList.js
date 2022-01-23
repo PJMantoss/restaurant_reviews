48187b1363d378a66f552650b7eb76802c4e8684
PJMantoss <joelptoss@gmail.com>
2022-01-23 18:51:41 +0100

    update list component

---
+++ frontend/src/components/RestaurantsList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RestaurantsList() {
+  return (
+    <div className="RestaurantsList">
+      Hello Universe!
+    </div>
+  );
+}
+
+export default RestaurantsList;
