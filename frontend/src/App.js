a5e6478829b124503959db953dbea4f61ba31095
PJMantoss <joelptoss@gmail.com>
2022-01-23 19:07:10 +0100

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,16 @@ import RestaurantsList from "./components/RestaurantsList";
 import Login from "./components/Login";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null)
+  }
+  
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
