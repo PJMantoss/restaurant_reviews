72df7b16e52dd5e5ab4000492c837eeb99509bf1
PJMantoss <joelptoss@gmail.com>
2022-01-24 22:06:04 +0100

    update restaurants

--- frontend/src/components/Restaurants.js
+++ frontend/src/components/Restaurants.js
@@ -3,6 +3,15 @@ import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
 
 const Restaurant = props => {
+  const initialRestaurantState = {
+    id: null,
+    name: "",
+    address: {},
+    cuisine: "",
+    reviews: []
+  };
+  const [restaurant, setRestaurant] = useState(initialRestaurantState);
+  
   return (
     <div className="App">
       Hello Universe!
