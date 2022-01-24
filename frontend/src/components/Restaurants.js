1b61ebccea6776aa0e730c061a6edce0426d4f64
PJMantoss <joelptoss@gmail.com>
2022-01-24 22:05:21 +0100

    update restaurants

--- frontend/src/components/Restaurants.js
+++ frontend/src/components/Restaurants.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantDataService from "../services/restaurant";
 import { Link } from "react-router-dom";
 
