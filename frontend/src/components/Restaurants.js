2d5e937a24c9bcf7bbd9c029cf54301b779a480a
PJMantoss <joelptoss@gmail.com>
2022-01-24 22:06:31 +0100

    update restaurants

--- frontend/src/components/Restaurants.js
+++ frontend/src/components/Restaurants.js
@@ -11,7 +11,19 @@ const Restaurant = props => {
     reviews: []
   };
   const [restaurant, setRestaurant] = useState(initialRestaurantState);
-  
+
+  const getRestaurant = id => {
+    RestaurantDataService.get(id)
+      .then(response => {
+        setRestaurant(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+
   return (
     <div className="App">
       Hello Universe!
