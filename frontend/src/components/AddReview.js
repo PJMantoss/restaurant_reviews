2a4746e3414dbe7776138724e5cebd25fecfcccb
PJMantoss <joelptoss@gmail.com>
2022-01-25 18:30:54 +0100

    update AddReview

--- frontend/src/components/AddReview.js
+++ frontend/src/components/AddReview.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import RestaurantDataService from "../services/restaurant";
 
 const AddReview = props => {
+  let initialReviewState = ""
+
+  let editing = false;
+
   return (
     <div className="App">
       Hello Universe!
